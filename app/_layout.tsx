4a9978cc6afa0d4c499b596b60c61d3b3489e6ce
samuelbahiru <samuelbahiru93@gmail.com>
2025-03-28 20:43:52 +0300

    Added auth and login and tabs features

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import InitialLayout from "@/components/InitialLayout";
+
+// cleark private key
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error("Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in .env");
+}
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-        <Stack screenOptions={{ headerShown: false }}></Stack>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+          <InitialLayout />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkProvider>
   );
 }
